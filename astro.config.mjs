3f852e35e23ec5861be2b22d450baab3936f7c04
tonicatOWO <tonicatowo@proton.me>
2026-04-05 22:54:35 +0800

    feat: add path aliases for components and refactor imports
    - Added Vite and TypeScript path aliases for components, layouts, utils, assets, styles, and consts.
    - Refactored all relevant import statements to use new aliases.
    - Moved components into `layout`, `ui`, and `utils` subfolders for improved structure.
    - Updated MDX and Astro files to use new import paths.

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,5 +8,20 @@ import svelte from '@astrojs/svelte';
 export default defineConfig({
 	site: 'https://example.com',
 	output: 'static',
-	integrations: [mdx(), sitemap(), svelte(), UnoCSS({ injectReset: true })]
+	integrations: [mdx(), sitemap(), svelte(), UnoCSS({ injectReset: true })],
+	vite: {
+		resolve: {
+			alias: {
+				'@components': '/src/components',
+				'@layout': '/src/components/layout',
+				'@ui': '/src/components/ui',
+				'@utils': '/src/components/utils',
+				'@layouts': '/src/layouts',
+				'@pages': '/src/pages',
+				'@consts': '/src/consts',
+				'@assets': '/src/assets',
+				'@styles': '/src/styles'
+			}
+		}
+	}
 });
