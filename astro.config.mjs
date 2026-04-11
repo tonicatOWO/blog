2ad6e9b5a874da736b2c3032e7512b6e7c73db25
tonicatOWO <tonicatowo@proton.me>
2026-04-11 14:02:08 +0800

    ci(deploy): add Forgejo workflow for Codeberg Pages & update site config
    - Add `.forgejo/workflows/deploy.yml` to automate deployment to Codeberg Pages on push to `main`
    - Update `astro.config.mjs`:
      - Change `site` URL to `https://tonicatowo.codeberg.page`
      - Update `base` path to `/blog`

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,8 @@ import pagefind from 'astro-pagefind';
 import svelte from '@astrojs/svelte';
 
 export default defineConfig({
-	site: 'https://tonicatowo.github.io/hosting_blog',
-	base: '/hosting_blog',
+	site: 'https://tonicatowo.codeberg.page',
+	base: '/blog',
 	output: 'static',
 	integrations: [mdx(), sitemap(), svelte(), UnoCSS({ injectReset: true }), pagefind()],
 	vite: {
