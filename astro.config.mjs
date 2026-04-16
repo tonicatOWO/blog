d396377190a734e2b2c51c2f2e1653e56e229a38
tonicatowo <tonicatowo@proton.me>
2026-04-16 17:38:30 +0800

    fix: add heroImage to BaseHead and configure dev server

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,41 +10,45 @@ import catppuccinIcons from '@iconify-json/catppuccin/icons.json';
 import svelte from '@astrojs/svelte';
 
 export default defineConfig({
-	site: 'https://blog.tonicatowo.xyz',
-	output: 'static',
-	trailingSlash: 'ignore',
-	integrations: [
-		mdx(),
-		sitemap(),
-		svelte(),
-		UnoCSS({
-			injectReset: true,
-			presets: [
-				presetIcons({
-					scale: 1.2,
-					warn: true,
-					collections: {
-						lucide: lucideIcons,
-						catppuccin: catppuccinIcons
-					}
-				})
-			]
-		}),
-		pagefind()
-	],
-	vite: {
-		resolve: {
-			alias: {
-				'@components': '/src/components',
-				'@layout': '/src/components/layout',
-				'@ui': '/src/components/ui',
-				'@utils': '/src/components/utils',
-				'@layouts': '/src/layouts',
-				'@pages': '/src/pages',
-				'@consts': '/src/consts',
-				'@assets': '/src/assets',
-				'@styles': '/src/styles'
-			}
-		}
-	}
+        site: 'https://blog.tonicatowo.xyz',
+        output: 'static',
+        trailingSlash: 'ignore',
+        integrations: [
+                mdx(),
+                sitemap(),
+                svelte(),
+                UnoCSS({
+                        injectReset: true,
+                        presets: [
+                                presetIcons({
+                                        scale: 1.2,
+                                        warn: true,
+                                        collections: {
+                                                lucide: lucideIcons,
+                                                catppuccin: catppuccinIcons
+                                        }
+                                })
+                        ]
+                }),
+                pagefind()
+        ],
+        vite: {
+                server: {
+                        host: true,
+                        allowedHosts: ['.ngrok-free.dev'],
+                },
+                resolve: {
+                        alias: {
+                                '@components': '/src/components',
+                                '@layout': '/src/components/layout',
+                                '@ui': '/src/components/ui',
+                                '@utils': '/src/components/utils',
+                                '@layouts': '/src/layouts',
+                                '@pages': '/src/pages',
+                                '@consts': '/src/consts',
+                                '@assets': '/src/assets',
+                                '@styles': '/src/styles'
+                        }
+                }
+        }
 });
